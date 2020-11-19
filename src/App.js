82a322d70a762c4cb57ddd7f74604884900cb9cd
Leslie Villatoro <levill98@gmail.com>
2020-11-18 19:47:14 -0800

    fix route

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/contact" component={Contact} />
         <Route exact path="/about" component={About} />
-        <Route exact path="*" component={Home} />
+        <Route exact path="*" redirect="/" />
     </Router>
   );
 }
