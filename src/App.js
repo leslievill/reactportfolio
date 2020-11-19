4316cd319dfcceef24000bce5b8fe876fa93a1a3
Leslie Villatoro <levill98@gmail.com>
2020-11-18 19:55:11 -0800

    fixing reactportfolio route'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import Contact from './pages/Contact';
@@ -8,13 +8,18 @@ import './App.css';
 
 function App() {
   return (
+  
     <Router>
-        <Route exact path="/" component={Home} />
+    <Switch>
+        <Route exact path={["/","/reactportfolio"]} component={Home} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/contact" component={Contact} />
         <Route exact path="/about" component={About} />
-        <Route exact path="*" redirect="/" />
+        
+    </Switch>
     </Router>
+
+    
   );
 }
 
